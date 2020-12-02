dd55dfe00b9cd222c41e7901ce43fb57f6de8d04
anettkaselo <anettsikka@gmail.com>
2020-12-02 18:04:19 +0200

    generated components

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './global/navbar/navbar.component';
+import { FooterComponent } from './global/footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { ItemListComponent } from './item/item-list/item-list.component';
+import { ItemViewComponent } from './item/item-view/item-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CartComponent,
+    ItemListComponent,
+    ItemViewComponent
   ],
   imports: [
     BrowserModule,
