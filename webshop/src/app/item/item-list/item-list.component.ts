f9c163f78c06090b66dcbe3cd90bb53790124bc1
anettkaselo <anettsikka@gmail.com>
2020-12-02 20:01:01 +0200

    dummy tooted avalehel

--- webshop/src/app/item/item-list/item-list.component.ts
+++ webshop/src/app/item/item-list/item-list.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
+ products = [
+   { title: "Tere või 82%", category: "Piimatoode",
+    price: 1.49, imgSrc: "https://ecoop.ee/media/cache/db/1f/95672-tere-voi-82-200g-01d1.png" },
+   { title: "Tere või 82%", category: "Piimatoode",
+   price: 1.49, imgSrc: "https://ecoop.ee/media/cache/db/1f/95672-tere-voi-82-200g-01d1.png" },
+   { title: "Tere või 82%", category: "Piimatoode",
+   price: 1.49, imgSrc: "https://ecoop.ee/media/cache/db/1f/95672-tere-voi-82-200g-01d1.png" },
+   { title: "Tere või 82%", category: "Piimatoode",
+   price: 1.49, imgSrc: "https://ecoop.ee/media/cache/db/1f/95672-tere-voi-82-200g-01d1.png" },
+  ];
+
 
   constructor() { }
 
