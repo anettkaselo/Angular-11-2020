95f8483c8260c9994f96566cf067e1eef39dd311
anettkaselo <anettsikka@gmail.com>
2020-12-16 19:44:39 +0200

    item view

--- webshop/src/app/item/item-view/item-view.component.ts
+++ webshop/src/app/item/item-view/item-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ItemService } from '../item.service';
 
 @Component({
@@ -7,10 +8,16 @@ import { ItemService } from '../item.service';
   styleUrls: ['./item-view.component.css']
 })
 export class ItemViewComponent implements OnInit {
+  products: any;
+  product: any;
+  id: any;
 
-  constructor() { }
+  constructor(private itemService: ItemService, private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
+    this.products = this.itemService.products;
+    this.id = this.route.snapshot.paramMap.get("itemId");
+    this.product = this.products[this.id]
   }
-
+ 
 }
